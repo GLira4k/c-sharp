ce8b8969870db301fb2a565716d343a21ad2c61d
GLira4k <103190663+GLira4k@users.noreply.github.com>
2024-08-07 10:50:28 -0300

    Primeiros passos

---
+++ Aulas/Tipos Primitivos/index.cs
@@ -0,0 +1,10 @@
+/* Tipos Primitivos (Também conhecidos como *built-in types*)
+
+    São classificados em:
+
+    - Tipos Simple (Simple Types)
+    - Enumeradores (Enums)
+    - Estruturas (Structs)
+    - Tipos Nulos (Nullable Types)
+
+*/
