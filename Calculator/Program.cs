e1b9480625c4e142395aeac973788659ecb7395c
GLira4k <103190663+GLira4k@users.noreply.github.com>
2024-08-20 18:21:12 -0300

    Create Program.cs

---
+++ Calculator/Program.cs
@@ -0,0 +1,15 @@
+using System;
+
+namespace MeuApp
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.Clear();
+            Console.WriteLine("Primeiro Valor: ");
+
+            float v1 = (float)Console.ReadLine();
+        }
+    }
+}
