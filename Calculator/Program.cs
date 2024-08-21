03875478f723902936d9c2b2f713120a008e84cf
GLira4k <103190663+GLira4k@users.noreply.github.com>
2024-08-21 11:55:49 -0300

    Update Program.cs

--- Calculator/Program.cs
+++ Calculator/Program.cs
@@ -6,10 +6,38 @@ namespace MeuApp
     {
         public static void Main(string[] args)
         {
+            
+            Subtracao();
+
+        }
+
+        static void Soma(){
             Console.Clear();
             Console.WriteLine("Primeiro Valor: ");
+            float v1 = float.Parse(Console.ReadLine());
 
-            float v1 = (float)Console.ReadLine();
+            Console.WriteLine("Segundo Valor: ");
+            float v2 = float.Parse(Console.ReadLine());
+
+            float resultado = v1 + v2;
+            // Console.WriteLine($"O resultado da soma é: {resultado}");
+            // Console.WriteLine($"O resultado da soma é: {v1 + v2}");
+            // Console.WriteLine("O resultado da soma é: " + (v1 + v2));
+            Console.WriteLine("O resultado da soma é: " + resultado);
+            Console.ReadKey();
+        }
+
+        static void Subtracao(){
+            Console.Clear();
+            Console.WriteLine("Primeiro Valor: ");
+            float v1 = float.Parse(Console.ReadLine());
+
+            Console.WriteLine("Segundo Valor: ");
+            float v2 = float.Parse(Console.ReadLine());
+
+            float resultado = v1 - v2;
+            Console.WriteLine("O resultado da subtração é: " + resultado);
+            Console.ReadKey();
         }
     }
 }
