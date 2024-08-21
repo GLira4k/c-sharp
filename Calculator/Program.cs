13c0378145c5293dbdd4a8b2a1a5b193b13063f3
GLira4k <103190663+GLira4k@users.noreply.github.com>
2024-08-21 18:12:44 -0300

    Update Program.cs

--- Calculator/Program.cs
+++ Calculator/Program.cs
@@ -6,12 +6,43 @@ namespace MeuApp
     {
         public static void Main(string[] args)
         {
+            Menu();
+        }
+
+        static void Menu()
+        {
+            Console.Clear();    
+            Console.WriteLine("Escolha uma opção: ");
+            Console.WriteLine("1 - Soma");
+            Console.WriteLine("2 - Subtração");
+            Console.WriteLine("3 - Multiplicação");
+            Console.WriteLine("4 - Divisão");
+
+            Console.WriteLine("------------");
             
-            Subtracao();
+            int op = int.Parse(Console.ReadLine());
+
+
+            switch(op)
+            {
+                case 1: Soma();
+                break;
+                case 2: Subtracao();
+                break;
+                case 3: Multiplicacao();
+                break;
+                case 4: Divisao();
+                break;
+                default: Console.WriteLine("Opção inválida");
+                Menu();
+                break;
+            }
+            
 
         }
 
-        static void Soma(){
+        static void Soma()
+        {
             Console.Clear();
             Console.WriteLine("Primeiro Valor: ");
             float v1 = float.Parse(Console.ReadLine());
@@ -24,10 +55,12 @@ namespace MeuApp
             // Console.WriteLine($"O resultado da soma é: {v1 + v2}");
             // Console.WriteLine("O resultado da soma é: " + (v1 + v2));
             Console.WriteLine("O resultado da soma é: " + resultado);
-            Console.ReadKey();
+            //Console.ReadKey();
+            Menu();
         }
 
-        static void Subtracao(){
+        static void Subtracao()
+        {
             Console.Clear();
             Console.WriteLine("Primeiro Valor: ");
             float v1 = float.Parse(Console.ReadLine());
@@ -37,7 +70,36 @@ namespace MeuApp
 
             float resultado = v1 - v2;
             Console.WriteLine("O resultado da subtração é: " + resultado);
-            Console.ReadKey();
+            // Console.ReadKey();
+            Menu();
+        }
+
+        static void Divisao()
+        {
+            Console.Clear();
+            Console.WriteLine("Primeiro Valor: ");
+            float v1 = float.Parse(Console.ReadLine());
+
+            Console.WriteLine("Segundo Valor: ");
+            float v2 = float.Parse(Console.ReadLine());
+
+            float resultado = v1 / v2;
+            Console.WriteLine("O resultado da divisão é: " + resultado);
+            Menu();
+        }
+
+        static void Multiplicacao()
+        {
+            Console.Clear();
+            Console.WriteLine("Primeiro Valor: ");
+            float v1 = float.Parse(Console.ReadLine());
+
+            Console.WriteLine("Segundo Valor: ");
+            float v2 = float.Parse(Console.ReadLine());
+
+            float resultado = v1 * v2;
+            Console.WriteLine("O resultado da multiplicação é: " + resultado);
+            Menu();
         }
     }
 }
