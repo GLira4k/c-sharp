c41f5aa3d9de8e66610fa9355ac87ed8a48c2a14
GLira4k <103190663+GLira4k@users.noreply.github.com>
2024-08-29 17:45:22 -0300

    Praticando

---
+++ Faturamento Distribuidora/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Linq;
+
+namespace FaturamentoDistribuidora
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            decimal[] faturamentoDiario = new decimal[365];
+
+            Random random = new Random();
+            for (int i = 0; i < faturamentoDiario.Length; i++)
+            {
+                faturamentoDiario[i] = (decimal)(random.Next(0, 1000) + random.NextDouble());
+            }
+
+            var diasComFaturamento = faturamentoDiario.Where(f => f > 0).ToArray();
+
+            decimal menorFaturamento = diasComFaturamento.Min();
+            Console.WriteLine("Menor valor de faturamento: " + menorFaturamento);
+
+            decimal maiorFaturamento = diasComFaturamento.Max();
+            Console.WriteLine("Maior valor de faturamento: " + maiorFaturamento);
+
+            decimal mediaFaturamento = diasComFaturamento.Average();
+            Console.WriteLine("Média de faturamento: " + mediaFaturamento);
+
+            int diasAcimaDaMedia = diasComFaturamento.Count(f => f > mediaFaturamento);
+            Console.WriteLine("Dias com faturamento acima da média: " + diasAcimaDaMedia);
+        }
+    }
+}
