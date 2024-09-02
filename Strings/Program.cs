4bc8a91adec6db41408ffe3636cb68f9ef2bce14
GLira4k <103190663+GLira4k@users.noreply.github.com>
2024-09-02 18:04:04 -0300

    Update Program.cs

--- Strings/Program.cs
+++ Strings/Program.cs
@@ -18,10 +18,12 @@ namespace MeuApp
     {
         public static void Main(string[] args)
         {
-            var id = Guid.NewGuid();
+            var id = Guid.NewGuid(); // Gera um novo guid
             id.ToString();
 
-            id = new Guid("efabd429-8d8e-46b5-b8be-bedc427c565f");
+            id = new Guid("efabd429-8d8e-46b5-b8be-bedc427c565f"); // Precisa está nessa formatação para rodar
+
+            id = new Guid(); // Irá gerar um guid só com zeros
 
             Console.WriteLine(id.ToString().Substring(0,8));
         }
