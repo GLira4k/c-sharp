97627e28b0ccf48cada6e74d48c8ccb467a024da
GLira4k <103190663+GLira4k@users.noreply.github.com>
2024-09-06 17:11:53 -0300

    Update Program.cs

--- Strings/Manipulando Strings/Program.cs
+++ Strings/Manipulando Strings/Program.cs
@@ -1,11 +1,19 @@
 /*
     Replace
 
+    Usado para trocar palavras dentro da string
+
     Split
     
+    Usado para transformar uma string em um array, a partir de um delimilador (geralmente virgula e espaço)
+
     Substring
 
+    seleciona uma cadeia de caracteres com base nos parâmetros passados no método, texto.Substring(10, 5) (a partir de qual índice, quantos caracteres selecionar)
+
     Trim
+
+    exclui os espaços no início e no final das strings
 */
 
 using System;
